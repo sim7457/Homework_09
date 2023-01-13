70c10a52fa04abccc596975d0e086ea2db50de5f
sim7457 <118698648+sim7457@users.noreply.github.com>
2023-01-13 15:02:57 +0900

    20230113 15:00

---
+++ js/sub.js
@@ -0,0 +1,18 @@
+$(function () {
+    AOS.init();
+    $('.sub_slick ul').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+        fade: true,
+        // variableWidth: true,
+    })
+
+
+
+
+
+
+
+
+})
